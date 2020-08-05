4b7906279dc56bac9cfd8e66454051e0ba06ea89
Peter Kazarinoff <peter.kazarinoff@pcc.edu>
2020-08-04 22:09:22 -0700

    added one return

--- publishconf.py
+++ publishconf.py
@@ -17,6 +17,7 @@ RELATIVE_URLS = True
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
 CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
 
+
 DELETE_OUTPUT_DIRECTORY = True
 
 # Following items are often useful when publishing
